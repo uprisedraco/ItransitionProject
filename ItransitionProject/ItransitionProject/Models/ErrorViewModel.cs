bf843893559e6cea2bd76f2c68029d4a9fda8c4f
uprisedraco <uprisedraco@gmail.com>
2021-12-29 18:57:26 +0200

    Start

--- ItransitionProject/ItransitionProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ItransitionProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
